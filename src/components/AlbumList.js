f7d08735dcf290f705cf557c351498d537ac691e
davidbarboza <benjigoenitz@gmail.com>
2018-09-24 22:35:18 -0500

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import AlbumDetail from './AlbumDetail';
+
+//lifecycle methods*
+class AlbumList extends Component {
+   //Component level state
+    state = { albums: [] };
+   
+    componentWillMount() {
+        fetch('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => response.json())
+            .then(responseData => this.setState({ albums: responseData }));
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album  => 
+            //best is the data has ID
+            <AlbumDetail key={album.title} album={album}/>
+        );
+    }
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <ScrollView>
+                {this.renderAlbums()}
+            </ScrollView>
+        );
+    }
+}
+
+export default AlbumList;
